12a72d9eb25d9407481640e894bef74484ac5292
Nihal00 <nihallsuvarna@gmail.com>
2024-07-27 00:16:55 +0530

    router demo

---
+++ routing-demo/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+    return (
+        <div className="about">
+            <h1>Welcome to my about page</h1>
+        </div>
+    )
+}
---
+++ routing-demo/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return (
+        <div className="home">
+            <h1>Welcome to my home page</h1>
+        </div>
+    )
+}
